5cecf44d255d9d6701e2f54de2c6119b41ea1939
peter <perter@126.com>
2019-07-31 18:56:50 +0800

    init

--- src/Controller/Pay361CallbackController.php
+++ src/Controller/Pay361CallbackController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class Pay361CallbackController extends AbstractController
 {
