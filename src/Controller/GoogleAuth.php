eb9fffe262e55d8030dd95f60caba1bf1c686fae
peter <perter@126.com>
2019-08-15 12:14:17 +0800

    init

---
+++ src/Controller/GoogleAuth.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\UserAccount;
+use Dbh\SfCoreBundle\Controller\BaseNeedLoginController;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Response\QrCodeResponse;
+
+class GoogleAuth extends BaseNeedLoginController
+{
+    public function qrcode($size = 300) {
+        $this->checkLogin();
+        $auth = new \App\Helper\GoogleAuth();
+        $user = $this->getUser();
+        if (!$user instanceof UserAccount) {
+            return 'failed';
+        }
+
+        $url = $auth->getQRCodeGoogleUrl('yub', $user->getGoogleSecret());
+
+        $qrCode = new QrCode();
+        $qrCode->setSize(300);
+        $qrCode->setEncoding('UTF-8');
+        $qrCode->setErrorCorrectionLevel(new ErrorCorrectionLevel(ErrorCorrectionLevel::MEDIUM));
+        return new QrCodeResponse($qrCode);
+    }
+}
