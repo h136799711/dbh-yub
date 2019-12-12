9767722b978f070a688bacda9d73353fb968affe
peter <perter@126.com>
2019-12-12 18:15:12 +0800

    init

--- src/Command/DypayCommand.php
+++ src/Command/DypayCommand.php
@@ -126,6 +126,7 @@ class DypayCommand extends Command
         $list0 = $this->service->queryBy($map, $paging, ["id" => 'asc']);
         $allList = array_merge($list, $list3, $list0);
         var_dump($allList);
+        $output->writeln("process ".count($allList));
         foreach ($allList as $vo) {
             $this->notify($vo['id']);
         }
